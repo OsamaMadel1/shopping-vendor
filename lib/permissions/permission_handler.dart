98943ea95124e63a758181b7f4391ce5baae593a
osama <ahmad405792@gmail.com>
2025-07-15 18:29:54 -0700

    صلاحيات للصور

--- lib/permissions/permission_handler.dart
+++ lib/permissions/permission_handler.dart
@@ -1,3 +1,36 @@
+// import 'package:permission_handler/permission_handler.dart';
+
+// class PermissionsRequester {
+//   /// طلب صلاحية الكاميرا
+//   static Future<bool> requestCameraPermission() async {
+//     final status = await Permission.camera.status;
+//     if (status.isGranted) {
+//       return true;
+//     } else {
+//       final result = await Permission.camera.request();
+//       return result.isGranted;
+//     }
+//   }
+
+//   /// طلب صلاحية قراءة التخزين
+//   static Future<bool> requestStoragePermission() async {
+//     final status = await Permission.photos.isGranted;
+//     if (status) {
+//       return true;
+//     } else {
+//       final result = await Permission.storage.request();
+//       return result.isGranted;
+//     }
+//   }
+
+//   /// طلب الصلاحيتين معًا
+//   static Future<bool> requestCameraAndStoragePermissions() async {
+//     final cameraGranted = await requestCameraPermission();
+//     final storageGranted = await requestStoragePermission();
+//     return cameraGranted && storageGranted;
+//   }
+// }
+import 'dart:io';
 import 'package:permission_handler/permission_handler.dart';
 
 class PermissionsRequester {
@@ -12,12 +45,16 @@ class PermissionsRequester {
     }
   }
 
-  /// طلب صلاحية قراءة التخزين
+  /// طلب صلاحية التخزين حسب إصدار Android
   static Future<bool> requestStoragePermission() async {
-    final status = await Permission.storage.status;
-    if (status.isGranted) {
-      return true;
+    if (Platform.isAndroid) {
+      // Android 13+
+      if (await Permission.photos.isGranted) return true;
+      final result = await Permission.photos.request();
+      return result.isGranted;
     } else {
+      // Android 12 وأقل
+      if (await Permission.storage.isGranted) return true;
       final result = await Permission.storage.request();
       return result.isGranted;
     }
